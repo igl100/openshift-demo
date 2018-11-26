6a2834f20548f8aff6bfcb51fc545a3077508615
Isaac Galvan Lanado <igalvan@MBP-de-Isaac.Home>
2018-11-25 22:47:01 -0600

    start method changed

--- microservice-demo-frontend/hello-from-nodejs-server.js
+++ microservice-demo-frontend/hello-from-nodejs-server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var serveStatic = require('serve-static');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1337;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var servePublic = serveStatic('public', {'index': ['index.html', 'index.htm']});
 
@@ -20,3 +20,27 @@ console.log('Server running at http://'+server_ip_address+':'+server_port);
 
 		
 		
+//var express = require('http');
+//var serveStatic = require('serve-static');
+//var app = express();
+//
+//var servePublic = serveStatic('public', {'index': ['index.html', 'index.htm']});
+//
+//// setup ports
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1339;
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+//
+//app.get('/', function(req, res) {
+//	console.log(req.method, req.url, 'HTTP'+req.httpVersion, req.headers);
+//
+//	res.setHeader('Access-Control-Allow-Origin', '*');
+//    
+//    servePublic(req, res, function nextHandler(req, res){
+//    });    
+//
+//});
+//
+//// server listens in on port
+//app.listen(server_port, server_ip_address, function () {
+//	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+//});
