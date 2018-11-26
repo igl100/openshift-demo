30175858c244582dc99af1613796dfeb5af5f5a5
Isaac Galvan Lanado <igalvan@MBP-de-Isaac.Home>
2018-11-25 21:37:35 -0600

    port changed to 8080

--- microservice-demo-frontend/hello-from-nodejs-server.js
+++ microservice-demo-frontend/hello-from-nodejs-server.js
@@ -11,5 +11,5 @@ http.createServer(function handler(req, res) {
     
     servePublic(req, res, function nextHandler(req, res){
     });    
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8080/');
