82d426ad7cf09a94fa3e96a47a5603fd4caa2d22
Isaac Galvan Lanado <igalvan@MBP-de-Isaac.Home>
2018-11-25 22:53:42 -0600

    start method changed

--- microservice-demo-frontend/hello-from-nodejs-server.js
+++ microservice-demo-frontend/hello-from-nodejs-server.js
@@ -17,30 +17,3 @@ var server = http.createServer(function handler(req, res) {
 });
 server.listen(server_port, server_ip_address);
 console.log('Server running at http://'+server_ip_address+':'+server_port);
-
-		
-		
-//var express = require('http');
-//var serveStatic = require('serve-static');
-//var app = express();
-//
-//var servePublic = serveStatic('public', {'index': ['index.html', 'index.htm']});
-//
-//// setup ports
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1339;
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-//
-//app.get('/', function(req, res) {
-//	console.log(req.method, req.url, 'HTTP'+req.httpVersion, req.headers);
-//
-//	res.setHeader('Access-Control-Allow-Origin', '*');
-//    
-//    servePublic(req, res, function nextHandler(req, res){
-//    });    
-//
-//});
-//
-//// server listens in on port
-//app.listen(server_port, server_ip_address, function () {
-//	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
-//});
