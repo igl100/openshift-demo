8611ac0233d6bef16baf6238179aec891a267a67
Isaac Galvan Lanado <igalvan@MBP-de-Isaac.Home>
2018-11-25 21:35:29 -0600

    start script added

--- microservice-demo-frontend/hello-from-nodejs-server.js
+++ microservice-demo-frontend/hello-from-nodejs-server.js
@@ -11,5 +11,5 @@ http.createServer(function handler(req, res) {
     
     servePublic(req, res, function nextHandler(req, res){
     });    
-}).listen(8080, '127.0.0.1');
+}).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
